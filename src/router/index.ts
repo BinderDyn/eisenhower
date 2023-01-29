bd274d23114b8e635cbc173f5bf418790aa209c5
BinderDyn <marvler@gmx.de>
2023-01-29 12:42:32 +0100

    Remove boilerplate from default project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LegalAndPrivacyView from "../views/LegalAndPrivacyView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,14 +9,19 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/legal-and-privacy",
+    name: "legal-and-privacy",
+    component: LegalAndPrivacyView,
   },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  // },
 ];
 
 const router = createRouter({
--- src/views/LegalAndPrivacyView.vue
+++ src/views/LegalAndPrivacyView.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="home"></div>
+  <h1>Legal and privacy</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "HomeView",
-  components: {},
+  name: "LegalAndPrivacy",
 });
 </script>
