491e05dc504b3f3c07e9c9be0f021e1e11fc5a00
BinderDyn <marvler@gmx.de>
2023-02-04 19:36:16 +0100

    [EIS-3] Local storage for tasks

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-    roots: ['src/components', 'src/stores'],
-    // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-    moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
-    transform: {
-        // '.*\\.(vue)$': 'vue-jest',
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testURL: 'http://localhost/',
-}
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  roots: ["src/components", "src/stores"],
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ["js", "ts", "json", "vue"],
+  transform: {
+    // '.*\\.(vue)$': 'vue-jest',
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testURL: "http://localhost/",
+};
