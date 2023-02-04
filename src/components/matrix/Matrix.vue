95fc9d854b6ab13b1aea701264c248690877c972
BinderDyn <marvler@gmx.de>
2023-02-05 00:11:11 +0100

    [EIS-11] Add legend

--- src/components/matrix/Matrix.vue
+++ src/components/matrix/Matrix.vue
@@ -2,17 +2,21 @@
   <div class="priority-area-wrapper">
     <div class="priority-area b" style="border-top-left-radius: 25px">
       <p class="priority-letter">B</p>
+      <div class="priority-legend">Important but not urgent</div>
     </div>
     <div class="priority-area a" style="border-top-right-radius: 25px">
       <p class="priority-letter">A</p>
+      <div class="priority-legend">Important and urgent</div>
     </div>
   </div>
   <div class="priority-area-wrapper">
     <div class="priority-area d" style="border-bottom-left-radius: 25px">
       <p class="priority-letter">D</p>
+      <div class="priority-legend">Not important and not urgent</div>
     </div>
     <div class="priority-area c" style="border-bottom-right-radius: 25px">
       <p class="priority-letter">C</p>
+      <div class="priority-legend">Not important but urgent</div>
     </div>
   </div>
 </template>
@@ -24,6 +28,7 @@ export default defineComponent({
   name: "MatrixComponent",
 });
 </script>
+
 <style lang="css">
 .priority-area-wrapper {
   display: flex;
@@ -41,6 +46,7 @@ export default defineComponent({
   flex-grow: 1;
   height: 100%;
   text-align: right;
+  position: relative;
 }
 
 .priority-letter {
@@ -50,11 +56,23 @@ export default defineComponent({
   font-weight: bold;
 }
 
+.priority-legend {
+  font-size: 1.2rem;
+  bottom: 10px;
+  left: 10px;
+  position: absolute;
+  width: fit-content;
+  font-weight: 600;
+  padding: 0 10px;
+  line-height: 1.3rem;
+}
+
 .priority-area.d {
   background-color: rgba(242, 255, 126, 0.158);
 }
 
-.priority-area.d > .priority-letter {
+.priority-area.d > .priority-letter,
+.priority-area.d > .priority-legend {
   color: rgba(255, 166, 2, 0.589);
 }
 
@@ -62,7 +80,8 @@ export default defineComponent({
   background-color: rgba(137, 255, 126, 0.158);
 }
 
-.priority-area.c > .priority-letter {
+.priority-area.c > .priority-letter,
+.priority-area.c > .priority-legend {
   color: rgba(21, 255, 0, 0.589);
 }
 
@@ -70,7 +89,8 @@ export default defineComponent({
   background-color: rgba(126, 214, 255, 0.158);
 }
 
-.priority-area.b > .priority-letter {
+.priority-area.b > .priority-letter,
+.priority-area.b > .priority-legend {
   color: rgba(0, 174, 255, 0.589);
 }
 
@@ -78,7 +98,8 @@ export default defineComponent({
   background-color: rgba(255, 106, 106, 0.158);
 }
 
-.priority-area.a > .priority-letter {
+.priority-area.a > .priority-letter,
+.priority-area.a > .priority-legend {
   color: rgba(255, 0, 0, 0.589);
 }
 </style>
